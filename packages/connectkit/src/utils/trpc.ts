ebd4c78c95a68c6321dc7ac7e7f3b4589b69a147
Nalin Bhardwaj <github@nibnalin.me>
2024-10-16 19:28:08 -0700

    trpc/server: drop AppRouter for now

--- packages/connectkit/src/utils/trpc.ts
+++ packages/connectkit/src/utils/trpc.ts
@@ -1,13 +1,14 @@
-import { AppRouter } from "@daimo/pay-api";
+// TODO: re-enable for dev only in rollup.
+// import { AppRouter } from "@daimo/pay-api";
 import { createTRPCClient, httpBatchLink } from "@trpc/client";
 
 // TODO: env var in build
 export const apiUrl = "https://pay-api.daimo.xyz";
 
-export const trpc = createTRPCClient<AppRouter>({
+export const trpc = createTRPCClient({
   links: [
     httpBatchLink({
       url: apiUrl,
     }),
   ],
-});
+}) as any;
