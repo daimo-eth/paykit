198075d63529572c13f954805f14e4e66ddd26a6
Nalin Bhardwaj <github@nibnalin.me>
2024-10-20 18:09:50 -0700

    make hooks and display optimistic

--- packages/connectkit/src/components/Pages/Confirmation/index.tsx
+++ packages/connectkit/src/components/Pages/Confirmation/index.tsx
@@ -28,8 +28,12 @@ const Confirmation: React.FC = () => {
   const { done, txURL, currency } = (() => {
     if (daimoPayOrder && daimoPayOrder.mode === DaimoPayOrderMode.HYDRATED) {
       if (
-        daimoPayOrder.destStatus === DaimoPayOrderStatusDest.CLAIMED ||
-        daimoPayOrder.destStatus === DaimoPayOrderStatusDest.FAST_FINISHED
+        daimoPayOrder.destStatus in
+        [
+          DaimoPayOrderStatusDest.FAST_FINISH_SUBMITTED, // Frontends are optimistic, assume submits will be successful
+          DaimoPayOrderStatusDest.FAST_FINISH_SUCCESSFUL,
+          DaimoPayOrderStatusDest.CLAIM_SUCCESSFUL,
+        ]
       ) {
         const txHash =
           daimoPayOrder.destFastFinishTxHash ?? daimoPayOrder.destClaimTxHash;
--- packages/connectkit/src/hooks/useDaimoPayStatus.ts
+++ packages/connectkit/src/hooks/useDaimoPayStatus.ts
@@ -1,4 +1,9 @@
-import { DaimoPayOrderMode, DaimoPayOrderStatusDest, DaimoPayOrderStatusSource, writeDaimoPayOrderID } from "@daimo/common";
+import {
+  DaimoPayOrderMode,
+  DaimoPayOrderStatusDest,
+  DaimoPayOrderStatusSource,
+  writeDaimoPayOrderID,
+} from "@daimo/common";
 import { useContext } from "../components/DaimoPay";
 
 export function useDaimoPayStatus() {
@@ -9,23 +14,32 @@ export function useDaimoPayStatus() {
     const order = paymentInfo.daimoPayOrder;
     const paymentId = writeDaimoPayOrderID(order.id);
     if (order.mode === DaimoPayOrderMode.HYDRATED) {
-      if (order.destStatus === DaimoPayOrderStatusDest.FAST_FINISHED || order.destStatus === DaimoPayOrderStatusDest.CLAIMED) {
+      if (
+        order.destStatus in
+        [
+          DaimoPayOrderStatusDest.FAST_FINISH_SUBMITTED, // Frontends are optimistic, assume submits will be successful
+          DaimoPayOrderStatusDest.FAST_FINISH_SUCCESSFUL,
+          DaimoPayOrderStatusDest.CLAIM_SUCCESSFUL,
+        ]
+      ) {
         return {
           status: "payment_completed" as const,
           paymentId,
-        }
-      } else if (order.sourceStatus != DaimoPayOrderStatusSource.WAITING_PAYMENT) {
+        };
+      } else if (
+        order.sourceStatus != DaimoPayOrderStatusSource.WAITING_PAYMENT
+      ) {
         return {
           status: "payment_started" as const,
           paymentId,
-        }
+        };
       }
     }
 
     return {
       status: "payment_pending" as const,
       paymentId,
-    }
+    };
   })();
 
   return status;
