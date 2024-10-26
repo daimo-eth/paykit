ddde4b10425bf1061c6bcd8e22bab6207aa32531
DC <dc@dcpos.ch>
2024-10-26 14:58:44 -0700

    paykit: fix useDaimoPayStatus

--- packages/connectkit/src/hooks/useDaimoPayStatus.ts
+++ packages/connectkit/src/hooks/useDaimoPayStatus.ts
@@ -2,41 +2,37 @@ import {
   DaimoPayIntentStatus,
   DaimoPayOrderMode,
   DaimoPayOrderStatusSource,
+  PaymentStatus,
   writeDaimoPayOrderID,
 } from "@daimo/common";
 import { useContext } from "../components/DaimoPay";
 
-export function useDaimoPayStatus() {
+export type DaimoPayStatus = {
+  paymentId: string;
+  status: PaymentStatus;
+};
+
+export function useDaimoPayStatus(): DaimoPayStatus | undefined {
   const { paymentInfo } = useContext();
 
+  if (!paymentInfo || !paymentInfo.daimoPayOrder) return undefined;
+  const order = paymentInfo.daimoPayOrder;
+  const paymentId = writeDaimoPayOrderID(order.id);
+
   const status = (() => {
-    if (!paymentInfo || !paymentInfo.daimoPayOrder) return undefined;
-    const order = paymentInfo.daimoPayOrder;
-    const paymentId = writeDaimoPayOrderID(order.id);
     if (order.mode === DaimoPayOrderMode.HYDRATED) {
-      if (order.intentStatus != DaimoPayIntentStatus.PENDING) {
-        return {
-          status:
-            order.intentStatus === DaimoPayIntentStatus.SUCCESSFUL
-              ? "payment_completed"
-              : "payment_bounced",
-          paymentId,
-        };
+      if (order.intentStatus !== DaimoPayIntentStatus.PENDING) {
+        return order.intentStatus === DaimoPayIntentStatus.SUCCESSFUL
+          ? PaymentStatus.Completed
+          : PaymentStatus.Bounced;
       } else if (
-        order.sourceStatus != DaimoPayOrderStatusSource.WAITING_PAYMENT
+        order.sourceStatus !== DaimoPayOrderStatusSource.WAITING_PAYMENT
       ) {
-        return {
-          status: "payment_started" as const,
-          paymentId,
-        };
+        return PaymentStatus.Started;
       }
     }
-
-    return {
-      status: "payment_pending" as const,
-      paymentId,
-    };
+    return PaymentStatus.Pending;
   })();
 
-  return status;
+  return { paymentId, status };
 }
