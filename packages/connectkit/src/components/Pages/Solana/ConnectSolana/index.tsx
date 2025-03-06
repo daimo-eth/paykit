a9b079352cb73e055ea3fcf53d439089479a2f7d
Gianluca Minoprio <gianlucaminoprio@icloud.com>
2025-03-06 16:22:42 -0500

    feat: add solana web connect flow for ios

--- packages/connectkit/src/components/Pages/Solana/ConnectSolana/index.tsx
+++ packages/connectkit/src/components/Pages/Solana/ConnectSolana/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 
 import {
   ModalContent,
@@ -7,14 +7,26 @@ import {
 } from "../../../Common/Modal/styles";
 
 import { useWallet } from "@solana/wallet-adapter-react";
+import { Phantom, Solflare } from "../../../../assets/logos";
 import Button from "../../../Common/Button";
 import OptionsList from "../../../Common/OptionsList";
 import { OrderHeader } from "../../../Common/OrderHeader";
 import { ROUTES, usePayContext } from "../../../DaimoPay";
-
+interface Option {
+  id: string;
+  title: string;
+  subtitle?: string;
+  icons: (React.ReactNode | string)[];
+  onClick: () => void;
+  disabled?: boolean;
+}
 const ConnectSolana: React.FC = () => {
   const { setSolanaConnector, setRoute } = usePayContext();
   const solanaWallets = useWallet();
+
+  const isOnIOS = useMemo(() => {
+    return /iPad|iPhone/.test(navigator.userAgent);
+  }, []);
 
   const options = solanaWallets.wallets.map((wallet) => ({
     id: wallet.adapter.name,
@@ -34,18 +46,68 @@ const ConnectSolana: React.FC = () => {
     },
   }));
 
+  const defaultOptions: Option[] = [
+    {
+      id: "phantom",
+      title: "Open in Phantom",
+      icons: [
+        <SquircleIcon
+          icon={(props) => <Phantom {...props} background />}
+          alt="Phantom"
+        />,
+      ],
+      onClick: () =>
+        window.open(
+          `https://phantom.app/ul/browse/${encodeURIComponent(window.location.href)}?ref=${encodeURIComponent(window.location.origin)}`,
+        ),
+    },
+    {
+      id: "solflare",
+      title: "Open in Solflare",
+      icons: [
+        <SquircleIcon
+          icon={(props) => <Solflare {...props} background />}
+          alt="Solflare"
+        />,
+      ],
+      onClick: () =>
+        window.open(
+          `https://solflare.com/ul/v1/browse/${encodeURIComponent(window.location.href)}?ref=${encodeURIComponent(window.location.origin)}`,
+          "_blank",
+        ),
+    },
+    // TODO: Add backpack when deeplink works
+    // {
+    //   id: "backpack",
+    //   title: "Open in Backpack",
+    //   icons: [
+    //     <SquircleIcon
+    //       icon={(props) => <Backpack {...props} background />}
+    //       alt="Backpack"
+    //     />,
+    //   ],
+    //   onClick: () =>
+    //     window.open(
+    //       `https://backpack.app/ul/v1/browse/${encodeURIComponent(window.location.href)}?ref=${encodeURIComponent(window.location.origin)}`,
+    //       "_blank",
+    //     ),
+    // },
+  ];
+
   return (
     <PageContent>
       <OrderHeader minified />
 
-      {solanaWallets.wallets.length === 0 && (
+      {solanaWallets.wallets.length === 0 && !isOnIOS && (
         <ModalContent
           style={{
             display: "flex",
+            flexDirection: "column",
             alignItems: "center",
             justifyContent: "center",
             paddingTop: 16,
             paddingBottom: 16,
+            gap: 16,
           }}
         >
           <ModalH1>No Solana wallets detected.</ModalH1>
@@ -59,15 +121,42 @@ const ConnectSolana: React.FC = () => {
         </ModalContent>
       )}
 
+      {isOnIOS && solanaWallets.wallets.length === 0 && (
+        <ModalContent
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 16,
+          }}
+        >
+          <ModalH1>Open this page and complete in your wallet</ModalH1>
+          <OptionsList options={defaultOptions} />
+        </ModalContent>
+      )}
       <OptionsList options={options} />
     </PageContent>
   );
 };
 
-const SquircleIcon = ({ icon, alt }: { icon: string; alt: string }) => (
-  <div style={{ borderRadius: "22.5%", overflow: "hidden" }}>
-    <img src={icon} alt={alt} />
-  </div>
-);
+const SquircleIcon = ({
+  icon,
+  alt,
+}: {
+  icon: string | React.ComponentType<any>;
+  alt: string;
+}) => {
+  const IconComponent =
+    typeof icon === "string" ? (
+      <img src={icon} alt={alt} />
+    ) : (
+      React.createElement(icon)
+    );
+
+  return (
+    <div style={{ borderRadius: "22.5%", overflow: "hidden" }}>
+      {IconComponent}
+    </div>
+  );
+};
 
 export default ConnectSolana;
